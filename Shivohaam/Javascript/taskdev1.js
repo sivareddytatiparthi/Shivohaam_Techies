402428e11187b90524ac88203d1d37e86454d043
sivareddytatiparthi <sivareddytatiparthi79@gamil.com>
2026-01-27 13:10:28 +0530

    i created new web page

---
+++ Shivohaam/Javascript/taskdev1.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("btn1");
+console.log(btn, "button");
+
+btn.addEventListener("click", function show() {
+  alert("Enter Your  Full  Details ");
+
+});
+let btn1 = document.getElementById("btnchoose");
+console.log(btn, "button");
+
+btn1.addEventListener("click", function show() {
+  alert(" choose Here : B.com, BSc ,Bzc , Mlt ,");
+});
+
+
+
